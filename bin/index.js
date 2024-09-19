2b951801c86244d5722dfc4c5de223b4475c2c14
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-09-19 20:33:06 +0530

    Resolved Suggested changes

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
-import fs from 'fs-extra';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { execSync } from 'child_process';
-import figlet from 'figlet';
-import chalk from 'chalk';
-import { createSpinner } from 'nanospinner';
-import { templates } from './configs.js';
+import { program } from "commander";
+import fs from "fs-extra";
+import path from "path";
+import { fileURLToPath } from "url";
+import { execSync } from "child_process";
+import figlet from "figlet";
+import chalk from "chalk";
+import { createSpinner } from "nanospinner";
+import { metadata, commands, templates } from "./configs.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,110 +16,117 @@ const __dirname = path.dirname(__filename);
 const template = templates.basic;
 
 program
-  .version('v1.0.0','-v, --version')
-  .description('A simple Express server generator');
+  .version(metadata.version, commands.version.versionFlags)
+  .description(metadata.description);
 
 program
-  .command('init')
-  .description('Initialize a new Express server')
+  .command(commands.init.command)
+  .description(commands.init.description)
   .action(() => {
-    
     toolIntro();
 
-    console.log('Starting server initialization...');
-    
-    const targetDir = process.cwd();
-    const parentDir = path.dirname(__dirname)
-    const templatePath = path.join(parentDir, 'templates',template.name);
-    const destinationPath = path.join(targetDir);
+    console.log("Starting server initialization...");
 
-    const initSpinner = createSpinner('Running npm init...').start()
+    const targetDir = process.cwd();
+    const parentDir = path.dirname(__dirname);
+    const templatePath = path.join(parentDir, "templates", template.name);
+    const destinationPath = path.join(targetDir);
+    const npmInit = chalk.bgWhite.black("npm init");
+
+    const initSpinner = createSpinner(`Running ${npmInit}...`).start();
     try {
       // execSync('npm init -y', { stdio: 'inherit', cwd: targetDir });
-      execSync('npm init -y', { stdio: 'ignore', cwd: targetDir });
-      initSpinner.success({ text: 'npm init completed successfully.' });
-    } catch (error) {
-      initSpinner.error({ text: `Error running npm init:\n` });
-      console.error(error.message);
+      execSync("npm init -y", { stdio: "ignore", cwd: targetDir });
+      initSpinner.success({ text: `${npmInit} completed successfully.` });
+    } catch (err) {
+      initSpinner.error({ text: `Error running ${npmInit}:\n` });
+      console.error(err.message);
       return;
     }
 
     //console.log(`Copying server template from ${templatePath} to ${destinationPath}`);
 
-    const copySpinner = createSpinner('Creating server files...').start();
+    const copySpinner = createSpinner("Creating server files...").start();
     try {
       fs.copySync(templatePath, destinationPath);
-      copySpinner.success({text:"Created server files successfully."});
-    } catch (error) {
-      copySpinner.error({text:"Error creating server files.\n"})
-      console.error(error.message);
+      copySpinner.success({ text: "Created server files successfully." });
+    } catch (err) {
+      copySpinner.error({ text: "Error creating server files.\n" });
+      console.error(err.message);
     }
 
-      
-    const addDependencies = createSpinner("Adding dependency packages...").start();
+    const addDependencies = createSpinner(
+      "Adding dependency packages..."
+    ).start();
     try {
-      // Update package.json to add express as a dependency
-      const packageJsonPath = path.join(targetDir, 'package.json');
-      const packageJsonContent = fs.readFileSync(packageJsonPath, 'utf8');
+      // Update package.json to add express as a dependency.
+      const packageJsonPath = path.join(targetDir, "package.json");
+      const packageJsonContent = fs.readFileSync(packageJsonPath, "utf8");
       const packageJson = JSON.parse(packageJsonContent);
       packageJson.dependencies = packageJson.dependencies || {};
       // packageJson.dependencies.express = "^4.17.1";
-      template.dependencies.forEach((dependency)=>{
+      template.dependencies.forEach((dependency) => {
         packageJson.dependencies[`${dependency.name}`] = dependency.version;
       });
       fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
 
-      addDependencies.success({text:'Added dependency packages successfully.'});
-    } catch(error) {
+      addDependencies.success({
+        text: "Added dependency packages successfully.",
+      });
+    } catch (err) {
       addDependencies.error("Error adding dependency packages.\n");
-      console.error(error.message);
+      console.error(err.message);
     }
-      
-    
-    const installDependencies = createSpinner("Installing dependency packages...").start();
+
+    const installDependencies = createSpinner(
+      "Installing dependency packages..."
+    ).start();
     try {
       //execSync('npm i', { stdio: 'inherit', cwd: targetDir });
-      execSync('npm i', { stdio: 'ignore', cwd: targetDir });
-      
-      installDependencies.success({text:'Installed dependencies successfully.'});
+      execSync("npm i", { stdio: "ignore", cwd: targetDir });
 
-      console.log(chalk.green.bold('\nSetup complete! To run your server:'));
-      console.log(chalk.yellow('Run:'), chalk.white.bold('npm start'));
-    } catch (error) {
-      installDependencies.error({text:'Error installing dependencies.\n'})
-      console.error(error);
+      installDependencies.success({
+        text: "Installed dependencies successfully.",
+      });
+
+      console.log(chalk.green.bold("\nSetup complete! To run your server:"));
+      console.log(chalk.yellow("Run:"), chalk.white.bold("npm start"));
+    } catch (err) {
+      installDependencies.error({ text: "Error installing dependencies.\n" });
+      console.error(err);
     }
   });
 
 program
-  .command('clear')
-  .description('Clear the directory')
-  .action(()=>{
+  .command(commands.clear.command)
+  .description(commands.clear.description)
+  .action(() => {
     const targetDir = process.cwd();
-    console.log('Clearing Directory...',chalk.bgRed.white(targetDir));
-    const clearingDirectory = createSpinner('Deleting All Files').start();
+    console.log("Clearing Directory...", chalk.bgRed.white(targetDir));
+    const clearingDirectory = createSpinner("Deleting All Files...").start();
     try {
-      // Read the directory
+      // Read the directory.
       const files = fs.readdirSync(targetDir);
-      
-      // Remove each file/directory except . and ..
+
       for (const file of files) {
         const filePath = path.join(targetDir, file);
         // if (file !== '.' && file !== '..') {
-          fs.removeSync(filePath);
+        fs.removeSync(filePath);
         // }
       }
-      
-      clearingDirectory.success({text:'Successfully cleared project directory'});
+
+      clearingDirectory.success({
+        text: "Successfully cleared project directory.",
+      });
     } catch (error) {
-      clearingDirectory.error({text:'Error clearing project directory'});
+      clearingDirectory.error({ text: "Error clearing project directory" });
       console.error(error);
     }
   });
 
 const toolIntro = () => {
   console.log(
-    figlet.textSync("Quick Start Express", {
+    figlet.textSync(metadata.name, {
       font: "Standard",
       horizontalLayout: "default",
       verticalLayout: "default",
@@ -127,8 +134,8 @@ const toolIntro = () => {
       whitespaceBreak: true,
     })
   );
-  
-  console.log(chalk.green.bold('A simple Express server generator CLI tool'));
+
+  console.log(chalk.green.bold(metadata.oneLineDescription));
 };
 
 program.parse(process.argv);
