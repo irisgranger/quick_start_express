7a1ff4022d19d9f23e4680371afb01153dd2a6b2
adripo <26493496+adripo@users.noreply.github.com>
2024-10-19 02:43:02 +0000

    fix: import for es6

--- templates/express_pg_sequelize/index.js
+++ templates/express_pg_sequelize/index.js
@@ -1,6 +1,8 @@
-require('dotenv').config();
 import express from 'express';
 import db from './db.js';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 3000;
